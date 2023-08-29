14c463aaf6119bd936346c56e6bbce90530241ab
RDJ ironman <ironman053103@gmail.com>
2023-08-29 12:58:44 +0530

    Dynamic agent_address_fetching

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import DHome from './pages/DHome/DHome';
 import Appointments from './components/Appointments';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createContext } from 'react';
+import Cookies from 'js-cookie';
 export const AgentAddressContext = createContext();
 
 function App() {
-  const agentAddress="0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const agentAddress=require('./Data/contract-address.json').contractAddress;
+  //console.log(agentAddress);
   return (
     <AgentAddressContext.Provider value={agentAddress}>
     <div>
